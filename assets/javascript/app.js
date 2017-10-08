aead4882370cd87bc0673fa223d515c0f1542862
Peter Peck <karatekid4christ@gmail.com>
2017-10-07 19:40:07 -0500

    Getting everything to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,92 @@
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var time = 31;
+var time = 46;
 var intervalId;
+var answer;
+var notanswered;
+var answerbank = [];
+
+//Uploads results to the HTML
+function uploadresult(){
+	$("#results").show();
+	$("#results").html("Correct: " + correct + "<br>" + "Incorrect: " + incorrect + "<br>" +"unanswered: " + unanswered + "<br>");
+};
+
+//see if the person check the radio inputs. Checks if the user inputs are the correct answer
+function check(){
+
+	for (var i = 0; i < answerbank.length; i++) {
+		answerbank[i] = parseInt(answerbank[i]);
+		if(answerbank[i] === 1){
+		correct++;
+		} 
+
+		else if(answerbank[i] === 0) {
+			incorrect++;
+		}
+	}
+	
+	notanswered = $("#question1 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+	notanswered = $("#question2 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+	notanswered = $("#question3 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+	notanswered = $("#question4 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+	notanswered = $("#question5 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+	notanswered = $("#question6 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+	notanswered = $("#question7 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+	notanswered = $("#question8 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+	notanswered = $("#question9 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+	notanswered = $("#question10 input").is(':checked');
+	if(!notanswered){
+		unanswered++;
+	}
+
+uploadresult();
+};
+
+
 
 //Starts timer
 function start() {
   intervalId = setInterval(count, 1000);
 };
+
 
 //Gives the interval of the count/timer
 function count() {
@@ -25,10 +104,9 @@ function count() {
 };
 
 
-
 //When button, "Start" is clicked, timer begins and is shown up on top and hides the button and shows the question
-
 $(document).ready(function(){
+
 	$("#start").click(function(){
 	$(this).hide();
 	$(".timer").addClass("timeleft");
@@ -38,33 +116,15 @@ $(document).ready(function(){
 });
 
 
-//This function is executed after the button, "Done" is clicked to see if the person check the radio inputs. Checks if the user inputs are the correct answer
+//Tet values from input radios
+	$('input').click(function() {
+		answer = $(this).val();
+		answerbank.push(answer);
+	});
 
-function check(){
-	console.log(form[0])
-// 	var type = document.getElementsByName("q");
-// 	var len = type.length;
-// 	var x;
-
-// 	for (var i = 0; i < len; i++) {
-// 		x = document.getElementById("answer").checked;
-// 	}
-
-
-//     if(){
-//     	correct++;
-//     	console.log(correct)
-//     }
-	
-// 	else {
-// 		unanswered++;
-// 		console.log(unanswered)
-// 	}
-};
 
 //When button, "Done" is clicked, it hides the button and calls the "check function"
-
-	$("#done").click(function(){
+$("#done").click(function(){
 	$(this).hide();
 	$(".content").hide();
 	clearTimeout(intervalId);
